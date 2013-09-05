712d89fe96833466b85269ad2feb8507a36469e8
tacahiroy <tacahiroy@gmail.com>
2013-09-05 17:10:55 +0900

    Add a function to create SSH tunnel

--- _zsh/func/utils.zsh
+++ _zsh/func/utils.zsh
@@ -7,30 +7,12 @@ function weechat {
   fi
 }
 
-## utils
-function tm_conn2srvs {
-  local SSH=/usr/bin/ssh
-  local MOS=$HOME/bin/mos
-  local SSH_CONFIG=$HOME/.ssh/config
-  local pat=$1
-  NODES=($(cat $HOME/bin/nodes.txt))
-  if [ -z "${pat}" ]; then
-    echo 'Usage: tm_conn2srvs PATTERN'
+function tssh_tunnel() {
+  if tmux list-windows -F '#I:#W' | grep '^99:jp-3.tnl' 2>&1 >/dev/null; then
+    echo already listening port 8103
     return
   fi
-
-  servers=($(echo ${NODES} | tr ' ' '\n' | grep "${pat}"))
-
-  for server in ${servers[@]}; do
-    if ! tmux list-windows | grep ${server}; then
-      if grep -q "^Host ${server}m$" ${SSH_CONFIG}; then
-        # echo mosh
-        tmux new-window -n ${server} "${MOS} ${server}"
-      else
-        # echo ssh
-        tmux new-window -n ${server} "${SSH} ${server}"
-      fi
-    fi
-  done
+  tmux neww -k -n jp-3.tnl -t 99 'ssh -C2qTnN -D 8103 takahiro@jp-3'
+  tmux select-window -l
 }
 
