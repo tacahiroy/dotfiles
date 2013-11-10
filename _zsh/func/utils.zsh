28571d58171073561f55a0e938c7859fe72ebe6e
tacahiroy <tacahiroy@gmail.com>
2013-11-11 01:42:36 +0900

    Change to use envvar

--- _zsh/func/utils.zsh
+++ _zsh/func/utils.zsh
@@ -2,7 +2,7 @@
 function weechat {
   if [ -x `which weechat-curses` ]; then
     if ! (ps aux | grep '[w]eechat-curses' 2>&1 > /dev/null); then
-      tmux new-window -t 9 weechat-curses
+      tmux new-window -t ${WEECHAT_TMUX_WINNUM:-98} weechat-curses
     fi
   fi
 }
