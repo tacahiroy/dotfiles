0ae2a687ba844d7211470f4c1ee23d2ea7bcc56e
Takahiro Yoshihara <tacahiroy@gmail.com>
2016-12-01 21:52:23 +0900

    Add some plugins

--- install.sh
+++ install.sh
@@ -8,9 +8,11 @@ else
 	READLINK='readlink -f'
 fi
 
+mkdir -p $HOME/bin
+
 for y in *; do
 	case $y in
-		README.md|UltiSnips|osx|install.sh|tmux.conf*|tmux-powerlinerc)
+		README.md|UltiSnips|osx|install.sh|tmux.conf*|tmux-powerlinerc|bin)
 			;;
 		*)
 			source=$y
@@ -20,9 +22,13 @@ for y in *; do
 	target=$HOME/.${source}
 
 	[ -L ${target} ] && unlink ${target}
-	ln -s $(${READLINK} ${source}) ${target}
+    ln -s $(${READLINK} ${source}) ${target}
 done
+exit
 
+##
+# tmux
+#
 if [ "${UN}" = 'linux' ]; then
     if cat /proc/version | grep Microsoft 2>&1 >/dev/null; then
         U=bow
@@ -38,3 +44,51 @@ TMUX_CONF=${TMUX_CONF}.${UN}
 target=$HOME/.tmux.conf
 [ -L ${target} ] && unlink ${target}
 ln -s $(${READLINK} ${TMUX_CONF}) ${target}
+
+cp bin/clipper $HOME/bin
+
+##
+# vim-plug
+#
+PLUG_VIM=$HOME/.vim/autoload/plug.vim
+if [ ! -r "${PLUG_VIM}" ]; then
+    curl -fLo ${PLUG_VIM} --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+##
+# Antibody
+#
+DOWNLOAD_URL="https://github.com/getantibody/antibody/releases/download"
+
+last_version() {
+  curl -s https://raw.githubusercontent.com/getantibody/homebrew-antibody/master/antibody.rb |
+    grep version |
+    cut -f2 -d '"'
+}
+
+download() {
+  version="$(last_version)" || true
+  test -z "$version" && {
+    echo "Unable to get antibody version."
+    exit 1
+  }
+  echo "Downloading antibody $version for $(uname -s)_$(uname -m)..."
+  rm -f /tmp/antibody.tar.gz
+  curl -s -L -o /tmp/antibody.tar.gz \
+    "$DOWNLOAD_URL/v$version/antibody_$(uname -s)_$(uname -m).tar.gz"
+}
+
+extract() {
+  tar -xf /tmp/antibody.tar.gz -C "$TMPDIR"
+}
+
+if [ ! -x $HOME/bin/antibody ]; then
+    echo Installing antibody
+    test -z "$TMPDIR" && TMPDIR="$(mktemp -d)"
+    download
+    extract || true
+    mv -f "$TMPDIR"/antibody $HOME/bin
+    which antibody
+    echo Done
+fi
