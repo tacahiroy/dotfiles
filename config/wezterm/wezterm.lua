37ecb573ebf51c6ceece06a59991e01da60c4ae5
YOSHIHARA Takahiro <tacahiroy@gmail.com>
2024-01-26 09:51:43 +0900

    Tweaks for look and feel

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,22 +2,31 @@ local wezterm = require 'wezterm';
 
 return {
     font = wezterm.font_with_fallback({
-        "PlemolJP Console NF",
+      { family = "PlemolJP Console NF", weight = "Regular", harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }, },
+        "PlemolJP Console NFJ",
         "Fira Code",
         "HackGenNerd Console",
+        "Consolas",
+        "monospace",
     }),
 
     font_size = 13.2,
     freetype_load_target = "Normal",
     freetype_render_target = "HorizontalLcd",
-    
-    -- color_scheme = "nord-light",
-    color_scheme = "Isotake",
 
-    default_prog = {"bash"},
+    default_cursor_style = "SteadyUnderline",
+    cursor_thickness = "0.1cell",
+    
+    color_scheme = "horizon-bright",
+    -- color_scheme = "Isotake",
+
+    default_prog = {"/usr/bin/bash", "-l"},
 
     use_fancy_tab_bar = true,
+    hide_tab_bar_if_only_one_tab = true,
+    tab_bar_at_bottom = true,
 
+    -- window_background_opacity = 1.0,
     window_padding = {
         left = 2,
         right = 2,
